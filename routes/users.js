dbd979ade70fa60a2ae782fca011bd0c9ae1afb1
Jeffrey <d5269357812@gmail.com>
2023-02-07 11:23:34 +0800

    前端：將user物件的key改成用英文 後端：完成search + create 的區塊鏈串接功能

--- routes/users.js
+++ routes/users.js
@@ -13,52 +13,178 @@ var router = express.Router();
 // const app = express()
 // const port = 3000
 
-let users = [
-  {
-    "帳號": "user1",
-    "密碼": "password1",
-    "狀態": "啟用",
-    "createdAt": new Date(),
-    "updatedAt": new Date()
-  },
-  {
-    "帳號": "user2",
-    "密碼": "password2",
-    "狀態": "禁用",
-    "createdAt": new Date(),
-    "updatedAt": new Date()
-  },
-  {
-    "帳號": "user3",
-    "密碼": "password3",
-    "狀態": "啟用",
-    "createdAt": new Date(),
-    "updatedAt": new Date()
-  }
-]
 
-// router.get('/', (req, res) => {
-//   res.send('Hello World!')
+const Web3 = require('web3');
+const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('HTTP://localhost:8545'));
+const contractAbi = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_account",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_password",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_status",
+        "type": "uint256"
+      }
+    ],
+    "name": "createUser",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_account",
+        "type": "string"
+      }
+    ],
+    "name": "deleteUser",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_account",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_password",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_status",
+        "type": "uint256"
+      }
+    ],
+    "name": "updateUser",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "accounts",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "name": "users",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "account",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "password",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "status",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "createdAt",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "updatedAt",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]; // 合約的 ABI（接口說明）
+const contractAddress = '0x592e5747fF6481323F7bD59F3B9D875AeAa3Df45'; // 合約的地址
+const contract = new web3.eth.Contract(contractAbi, contractAddress);
+const privateKey = 'd12641050545136f0e86ba2c695a1e97c45715c01257220bc7c24e00f601b1d3'
+const pubKey = '0x1C9D9fB55779499F0ebCf700ac4b6EC183DD7b2a'
+
+
+
+let 帳號表 = []
+let users = []
+
+sync_chaindata()
+router.get('/', async (req, res) => {
+  await sync_chaindata()
+  res.render('user_management',{users,contractAddress});
+})
+
+// router.get('/:account', async (req, res) => {
+//   // const user = users.find(user => user.帳號 === req.params.account)
+//   const user = await contract.methods.users(req.params.account).call()
+//   if (!user) {
+//     res.status(404).send('User not found')
+//     return
+//   }
+//   res.json(user)
 // })
 
-router.get('/', (req, res) => {
-  // res.json(users)
-  res.render('user_management',{users});
-})
+router.post('/', async (req, res) => {
+  const send = async() => {
+    const tx = {
+      from:pubKey,
+      to:contractAddress,
+      // gas:50000,
+      gas:303146,
+      data:contract.methods.createUser(req.body.account, req.body.password, req.body.status).encodeABI()
+    }
+    const signature = await web3.eth.accounts.signTransaction(tx,privateKey)
+    web3.eth.sendSignedTransaction(signature.rawTransaction)
+        .on(
+        "receipt", (receipt) => {
+          console.log(receipt)
+        }
+        )
+        .catch(error => {
+          console.error(error)
+        })
 
-router.get('/:account', (req, res) => {
-  const user = users.find(user => user.帳號 === req.params.account)
-  if (!user) {
-    res.status(404).send('User not found')
-    return
   }
-  res.json(user)
-})
-
-router.post('/', (req, res) => {
-  const user = req.body
-  users.push(user)
-  res.json(user)
+  await send()
+  return
 })
 
 router.put('/:account', (req, res) => {
@@ -82,12 +208,26 @@ router.delete('/:account', (req, res) => {
   res.json(user)
 })
 
-// router.get('/user_management',(req,res)=>{
-//   res.render('user_management',{users});
-// })
 
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
+async function sync_chaindata(){
+  users = []
+  帳號表 = []
+  var i = 0;
+  var limit = 100;
+
+  while (i<limit){
+    var result
+    try {
+      result = await contract.methods.accounts(i).call()
+    }catch (e){
+      break
+    }
+    帳號表.push(result)
+    const result2 = await contract.methods.users(result).call()
+    users.push(result2)
+    i++
+  }
+
+}
 module.exports = router;
