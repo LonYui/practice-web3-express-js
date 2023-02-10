777e31b7086a1dff0e6e234704478c34d27e79fc
Jeffrey <d5269357812@gmail.com>
2023-02-10 16:36:33 +0800

    串接心abi介面

--- routes/users.js
+++ routes/users.js
@@ -17,212 +17,71 @@ var router = express.Router();
 const Web3 = require('web3');
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('HTTP://localhost:8545'));
-// web3.setProvider(new web3.providers.HttpProvider('https://ethereum-sepolia-rpc.allthatnode.com'));
-// const contractAbi = [
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "uint256",
-//         "name": "",
-//         "type": "uint256"
-//       }
-//     ],
-//     "name": "accounts",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "stateMutability": "view",
-//     "type": "function",
-//     "constant": true
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "users",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "account",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "string",
-//         "name": "password",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "uint256",
-//         "name": "status",
-//         "type": "uint256"
-//       },
-//       {
-//         "internalType": "uint256",
-//         "name": "createdAt",
-//         "type": "uint256"
-//       },
-//       {
-//         "internalType": "uint256",
-//         "name": "updatedAt",
-//         "type": "uint256"
-//       }
-//     ],
-//     "stateMutability": "view",
-//     "type": "function",
-//     "constant": true
-//   },
-//   {
-//     "inputs": [],
-//     "name": "initialize",
-//     "outputs": [],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "_account",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "string",
-//         "name": "_password",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "createUser",
-//     "outputs": [],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "_account",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "string",
-//         "name": "_password",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "updateUser",
-//     "outputs": [],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "_account",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "deleteUser",
-//     "outputs": [],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [],
-//     "name": "getAccountLength",
-//     "outputs": [
-//       {
-//         "internalType": "uint256",
-//         "name": "",
-//         "type": "uint256"
-//       }
-//     ],
-//     "stateMutability": "view",
-//     "type": "function",
-//     "constant": true
-//   }
-// ];//0x08abf1bC52427fe8b648cB6d70a7c646ef0C4411 // 合約的 ABI（接口說明）V1
 const contractAbi = [
   {
     "inputs": [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "accounts",
-    "outputs": [
+        "internalType": "string",
+        "name": "_account",
+        "type": "string"
+      },
       {
         "internalType": "string",
-        "name": "",
+        "name": "_password",
         "type": "string"
       }
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    "name": "C",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
     "inputs": [
       {
         "internalType": "string",
-        "name": "",
+        "name": "search_word",
         "type": "string"
       }
     ],
-    "name": "users",
+    "name": "R",
     "outputs": [
       {
-        "internalType": "string",
-        "name": "account",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "password",
-        "type": "string"
-      },
-      {
-        "internalType": "uint256",
-        "name": "status",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "createdAt",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "updatedAt",
-        "type": "uint256"
+        "components": [
+          {
+            "internalType": "string",
+            "name": "account",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "password",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "status",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "createdAt",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "updatedAt",
+            "type": "uint256"
+          }
+        ],
+        "internalType": "struct CRUD.User[]",
+        "name": "",
+        "type": "tuple[]"
       }
     ],
     "stateMutability": "view",
     "type": "function",
     "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "helloV2",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
   },
   {
     "inputs": [
@@ -237,25 +96,7 @@ const contractAbi = [
         "type": "string"
       }
     ],
-    "name": "createUser",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "_account",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "_password",
-        "type": "string"
-      }
-    ],
-    "name": "updateUser",
+    "name": "U",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
@@ -268,175 +109,16 @@ const contractAbi = [
         "type": "string"
       }
     ],
-    "name": "deleteUser",
+    "name": "D",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getAccountLength",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
   }
-];//0x08abf1bC52427fe8b648cB6d70a7c646ef0C4411 // 合約的 ABI（接口說明）V2
-
-// const contractAbi = [
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "_account",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "string",
-//         "name": "_password",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "createUser",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "_account",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "deleteUser",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "_account",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "string",
-//         "name": "_password",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "updateUser",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "stateMutability": "nonpayable",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "uint256",
-//         "name": "",
-//         "type": "uint256"
-//       }
-//     ],
-//     "name": "accounts",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "stateMutability": "view",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [],
-//     "name": "getAccountLength",
-//     "outputs": [
-//       {
-//         "internalType": "uint256",
-//         "name": "",
-//         "type": "uint256"
-//       }
-//     ],
-//     "stateMutability": "view",
-//     "type": "function"
-//   },
-//   {
-//     "inputs": [
-//       {
-//         "internalType": "string",
-//         "name": "",
-//         "type": "string"
-//       }
-//     ],
-//     "name": "users",
-//     "outputs": [
-//       {
-//         "internalType": "string",
-//         "name": "account",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "string",
-//         "name": "password",
-//         "type": "string"
-//       },
-//       {
-//         "internalType": "uint256",
-//         "name": "status",
-//         "type": "uint256"
-//       },
-//       {
-//         "internalType": "uint256",
-//         "name": "createdAt",
-//         "type": "uint256"
-//       },
-//       {
-//         "internalType": "uint256",
-//         "name": "updatedAt",
-//         "type": "uint256"
-//       }
-//     ],
-//     "stateMutability": "view",
-//     "type": "function"
-//   }
-// ]; // 合約的 ABI（接口說明） //0x5592cd2c816e86E4F9f2f9D3AFc2e1c4b8E01fDc
-
-// const proxyadminAddress = '0x4f312ec33adb3d16d358deac8ef4a699a829eb1d';//proxy管理員
+]
 const proxyadminAddress = '0x8964Eb9793747C7204603a2F6B3AEE11dCECB9e4';//proxy管理員
 const proxyadminAbi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"contract TransparentUpgradeableProxy","name":"proxy","type":"address"},{"internalType":"address","name":"newAdmin","type":"address"}],"name":"changeProxyAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract TransparentUpgradeableProxy","name":"proxy","type":"address"}],"name":"getProxyAdmin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"contract TransparentUpgradeableProxy","name":"proxy","type":"address"}],"name":"getProxyImplementation","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract TransparentUpgradeableProxy","name":"proxy","type":"address"},{"internalType":"address","name":"implementation","type":"address"}],"name":"upgrade","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract TransparentUpgradeableProxy","name":"proxy","type":"address"},{"internalType":"address","name":"implementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeAndCall","outputs":[],"stateMutability":"payable","type":"function"}]
 const proxyadminContract = new web3.eth.Contract(proxyadminAbi, proxyadminAddress);
-// const proxyAddress = '0x0CC3bF148Be8f776904E1f539552d55C0b5AA983';//proxy 測試練
-// const proxyAddress = '0xB104c407df01FE7D8136dA1864E1835321250674';//proxy 1
-const proxyAddress = '0xFE54C57E0a857c2C111373dF8E5f79566CAF3A63';//proxy 2
+const proxyAddress = '0x21A3f4644c92E5dc9ab1A604f92e8776060552D5';//proxy 2
 const contractAddress = proxyAddress; // 下方串接合約的地址
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
 const privateKey = 'd12641050545136f0e86ba2c695a1e97c45715c01257220bc7c24e00f601b1d3'
@@ -448,20 +130,12 @@ let 帳號表 = []
 let users = []
 
 router.get('/', async (req, res) => {
-  await sync_chaindata()
+  users = await contract.methods.R('').call()
+  // 篩選掉空的資料 此為合約介面的 workaround
+  users = users.filter(user => user.some(value => value.trim() !== '' && value!== '0'));//and so onnn
   const currentAddress = await proxyadminContract.methods.getProxyImplementation(proxyAddress).call()
   res.render('user_management',{users,contractAddress,currentAddress});
 })
-
-// router.get('/:account', async (req, res) => {
-//   // const user = users.find(user => user.帳號 === req.params.account)
-//   const user = await contract.methods.users(req.params.account).call()
-//   if (!user) {
-//     res.status(404).send('找不到使用者 #memory')
-//     return
-//   }
-//   res.json(user)
-// })
 
 router.post('/', async (req, res) => {
   const userIndex = users.findIndex(user => user.account === req.body.account)
@@ -476,7 +150,7 @@ router.post('/', async (req, res) => {
       to:contractAddress,
       // gas:50000,
       gas:303146,
-      data:contract.methods.createUser(req.body.account, req.body.password).encodeABI()
+      data:contract.methods.C(req.body.account, req.body.password).encodeABI()
     }
     const signature = await web3.eth.accounts.signTransaction(tx,privateKey)
     web3.eth.sendSignedTransaction(signature.rawTransaction)
@@ -509,7 +183,7 @@ router.put('/:account', async (req, res) => {
       to:contractAddress,
       // gas:50000,
       gas:303146,
-      data:contract.methods.updateUser(req.body.account, req.body.password).encodeABI()
+      data:contract.methods.U(req.body.account, req.body.password).encodeABI()
     }
     const signature = await web3.eth.accounts.signTransaction(tx,privateKey)
     web3.eth.sendSignedTransaction(signature.rawTransaction)
@@ -543,7 +217,7 @@ router.delete('/:account', async (req, res) => {
       to:contractAddress,
       // gas:50000,
       gas:303146,
-      data:contract.methods.deleteUser(req.params.account).encodeABI()
+      data:contract.methods.D(req.params.account).encodeABI()
     }
     const signature = await web3.eth.accounts.signTransaction(tx,privateKey)
     web3.eth.sendSignedTransaction(signature.rawTransaction)
@@ -563,26 +237,4 @@ router.delete('/:account', async (req, res) => {
   res.json({status: true, resultMessage: '刪除成功'});
 })
 
-
-
-async function sync_chaindata(){
-  users = []
-  帳號表 = []
-  var i = 0;
-  var limit = 100;
-
-  while (i<limit){
-    var result
-    try {
-      result = await contract.methods.accounts(i).call()
-    }catch (e){
-      break
-    }
-    帳號表.push(result)
-    const result2 = await contract.methods.users(result).call()
-    users.push(result2)
-    i++
-  }
-
-}
 module.exports = router;
